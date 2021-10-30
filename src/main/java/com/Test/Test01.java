418c327708372a8781128beae87d77ba6f74ba35
WangYuKai <2119859089@qq.com>
2021-10-30 18:05:25 +0800

    first

---
+++ src/main/java/com/Test/Test01.java
@@ -0,0 +1,11 @@
+package com.Test;
+
+/**
+ * @author shkstart
+ * @create 2021-10-30 18:01
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("张婉玉是傻逼");
+    }
+}
